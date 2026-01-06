6dcbdaaa724ba930a8e98db50cbdc655fa6133fa
thausale <cnops_niels@hotmail.com>
2026-01-06 23:57:26 +0100

    Configures email provider for Amazon SES
    Adds the @strapi/provider-email-amazon-ses plugin and configures it to use AWS SES for sending emails.
    
    This enables the application to send emails via Amazon SES by using environment variables for credentials and region.
    
    Sets default 'from' and 'reply-to' addresses.

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = {
+    email: {
+      config: {
+        provider: '@strapi/provider-email-amazon-ses',
+        providerOptions: {
+          key: process.env.AWS_SES_ACCESS_KEY,
+          secret: process.env.AWS_SES_SECRET_KEY,
+          amazon: `https://email.${process.env.AWS_SES_REGION}.amazonaws.com`
+        },
+        settings: {
+          defaultFrom: 'noreply@osrs-snowflakes.com',
+          defaultReplyTo: 'noreply@osrs-snowflakes.com'
+        }
+      }
+    }
+  };
